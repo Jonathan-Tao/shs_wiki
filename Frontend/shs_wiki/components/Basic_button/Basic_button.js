4000ab5a335b0abe4f9f88fa91664cc803c374bc
Playbird12 <83607406+Jonathan-Tao@users.noreply.github.com>
2022-01-22 21:54:39 -0600

    added counter button component

--- Frontend/shs_wiki/components/Basic_button/Basic_button.js
+++ Frontend/shs_wiki/components/Basic_button/Basic_button.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Button, ButtonGroup, useControllableState, Box } from '@chakra-ui/react';
 
 export default function Basic_button() {
-  return <div>
-      <Button colorScheme='green' onClick={console.log("fun times")}>Button</Button>
-  </div>;
+    const [value, setValue] = useControllableState({ defaultValue: 40 })
+    return (
+      <div>
+        <Button onClick={() => setValue(value + 1)}>+</Button>
+        <Box as='span' w='200px' mx='24px'>
+          {value}
+        </Box>
+        <Button onClick={() => setValue(value - 1)}>-</Button>
+      </div>
+    )
 }
