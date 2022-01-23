0b5b3f52f36cc534e6409018bbf293733219a60d
Playbird12 <83607406+Jonathan-Tao@users.noreply.github.com>
2022-01-22 21:27:04 -0600

    added basic button as proof of concept

---
+++ Frontend/shs_wiki/components/Basic_button/Basic_button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+export default function Basic_button() {
+  return <div>
+      <Button colorScheme='blue'>Button</Button>
+  </div>;
+}
