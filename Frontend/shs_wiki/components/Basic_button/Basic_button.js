fea079664d256f94700d6730a6c3076a590f4d27
Playbird12 <83607406+Jonathan-Tao@users.noreply.github.com>
2022-01-22 22:12:04 -0600

    spacing from main

--- Frontend/shs_wiki/components/Basic_button/Basic_button.js
+++ Frontend/shs_wiki/components/Basic_button/Basic_button.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Button, ButtonGroup, useControllableState, Box } from '@chakra-ui/react';
+import { Button, ButtonGroup, useControllableState, Box, text } from '@chakra-ui/react';
 
 export default function Basic_button() {
     const [value, setValue] = useControllableState({ defaultValue: 40 })
     return (
       <div>
+        <Box as='span' w='10px' mx = '24'></Box>
         <Button onClick={() => setValue(value + 1)}>+</Button>
         <Box as='span' w='200px' mx='24px'>
           {value}
